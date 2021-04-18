e0cd2ef4e08e1b070ec68cb360c8d191d4781fcb
yogawe1 <46183348+yogawe1@users.noreply.github.com>
2021-04-18 19:33:07 +0800

    Update baidu_speed.js

--- Task/baidu_speed.js
+++ Task/baidu_speed.js
@@ -25,7 +25,7 @@ let baiducash = $.getdata(`cash_baidu`);
 
 let taskON = $.getdata(`task_baidu`)||"true"//除提现和兑换外其他任务开关;
 let isblack = "false";
-let UA = $.getdata('bd_Agent')||'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 SP-engine/2.24.0 matrixstyle/0 matrixstyle/0 info baiduboxapp/5.4.0.10 (Baidu; P2 14.4.2)';
+let UA = $.getdata('bd_Agent')||'Mozilla/5.0 (iPhone; CPU iPhone OS 14_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 SP-engine/2.24.0 matrixstyle/0 light/1.0(WKWebView) themeUA=Theme/default info baiduboxapp/5.1.6.10 (Baidu; P2 14.2)';
 
 if (isGetCookie = typeof $request !== 'undefined') {
     GetCookie();
